0a66e05fa630b179abc4bee6e544d0783c0d1cf4
jamal_dt <carldeltoro@gmail.com>
2021-12-06 19:30:42 -0400

    Angular movies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
 import { Component } from "@angular/core";
 
+type MovieType = {
+  name: String;
+  available: number;
+  quantity: number;
+};
+type MoviesType = Array<MovieType>;
+
 @Component({
   selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"]
+  templateUrl: "./app.component.html"
 })
 export class AppComponent {
-  title = "Peliculas";
+  title: String = "Películas";
+  movies: MoviesType = [
+    {
+      name: "Avengers",
+      available: 3,
+      quantity: 0
+    },
+    {
+      name: "Terminator",
+      available: 5,
+      quantity: 0
+    }
+  ];
+
+  addMovieTicket(movieName) {
+    const movieIndex = this.movies.findIndex(
+      (movie) => movie.name === movieName
+    );
+
+    this.movies[movieIndex].quantity += 1;
+  }
+
+  removeMovieTicket(movieName) {
+    const movieIndex = this.movies.findIndex(
+      (movie) => movie.name === movieName
+    );
+
+    this.movies[movieIndex].quantity -= 1;
+  }
 }
